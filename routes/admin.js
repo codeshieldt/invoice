51350d007875486eb3b627561880bb5bd2c70650
Dilawar Khan Niazi <dilawar.niazi.dn@gmail.com>
2022-09-01 11:46:41 +0500

    final

--- routes/admin.js
+++ routes/admin.js
@@ -94,42 +94,46 @@ app.put('/client/createInvoice', auth, async(req, res) => {
     let month = dt.getMonth();
     let year =  dt.getFullYear();
 
-    month++;
-    due_date = date + '-' + month + '-' + year;
+    if(date == 5)
+    {
+        month++;
+        due_date = date + '-' + month + '-' + year;
 
-    totalAmount = Math.floor(Math.random() * 50000);
-    let percentage = totalAmount * 0.2;
-    totalAmountDue = totalAmount + percentage;
+        totalAmount = Math.floor(Math.random() * 50000);
+        let percentage = totalAmount * 0.2;
+        totalAmountDue = totalAmount + percentage;
 
-    let invoice = await new Invoice({
-        due_date,
-        totalAmount,
-        totalAmountDue
-    });
-    
-    invoice = await invoice.save();
+        let invoice = await new Invoice({
+            due_date,
+            totalAmount,
+            totalAmountDue
+        });
+        
+        invoice = await invoice.save();
 
-    client = await Client.findOneAndUpdate({email},
-        {
-            invoice
-        }, { new: true });
+        client = await Client.findOneAndUpdate({email},
+            {
+                invoice
+            }, { new: true });
 
 
-    res.send(client);
+        res.send(client);
+    }
+
+    res.send('Dues left to pay!')
 });
 
 app.post('/sendInvoice/:id', auth, async(req, res) => {
     let client = await Client
                         .findById(req.params.id)
                         .populate('invoice');
-    console.log(client.invoice.due_date);
 
     let dt = new Date;
     let date = dt.getDate();
     let month = dt.getMonth();
 
-    clientDate = Number(client.invoice.due_date.slice(0,2));
-    clientMonth = Number(client.invoice.due_date.slice(3,4));   //2-10-2022
+    clientDate = Number(client.invoice.due_date.slice(0,1));
+    clientMonth = Number(client.invoice.due_date.slice(2,4));   //2-10-2022
 
     if(month > clientMonth)
     {
